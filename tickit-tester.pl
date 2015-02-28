13ad9a0a78f35af2bdd13017d67ff2c0a621ad7f
Tom Molesworth <tom@perlsite.co.uk>
2015-02-28 17:28:09 +0000

    Turn off local echo by default, needs a reset(1) to work
    git-svn-id: http://entitymodel.com/svn/trunk/Tickit-Widget-Term@2952 c5bf8c3b-bf20-4ed3-b413-d5547a49f493

--- tickit-tester.pl
+++ tickit-tester.pl
@@ -159,14 +159,14 @@ sub on_key {
 	if($ev->type eq 'key') {
 		if($ev->str eq 'Enter') {
 			$self->pty->write("\n");
-			$self->push_text("\n");
+#			$self->push_text("\n");
 		} elsif($ev->str eq 'Backspace') {
 			$self->pty->write("\x08");
 #			$self->push_text("\n");
 		}
 	} elsif($ev->type eq 'text') {
 		$self->pty->write($ev->str);
-		$self->push_text($ev->str);
+#		$self->push_text($ev->str);
 	}
 	$self->redraw;
 }
