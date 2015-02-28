c9229b061a62626b4a3679ae5094374a68f7444c
Tom Molesworth <tom@perlsite.co.uk>
2015-02-28 17:28:08 +0000

    Various hacked-in VT102 features for basic top(1) rendering
    git-svn-id: http://entitymodel.com/svn/trunk/Tickit-Widget-Term@2945 c5bf8c3b-bf20-4ed3-b413-d5547a49f493

--- tickit-tester.pl
+++ tickit-tester.pl
@@ -14,6 +14,7 @@ use Variable::Disposition qw(retain_future);
 use IO::Tty;
 use IO::Pty;
 use Log::Any qw($log);
+use Tickit::Utils qw(textwidth);
 
 sub new {
 	my ($class, %args) = @_;
@@ -34,11 +35,18 @@ sub cols { 80 }
 sub render_to_rb {
 	my ($self, $rb, $rect) = @_;
 	my $win = $self->window;
+	$rb->clip($rect);
 
-	my $line = 0;
 	for my $item (@{$self->{writable}}) {
-		my $txt = $item =~ s/[^[:print:]]+//gr;
-		$rb->text_at($line++, 0, $txt, $self->get_style_pen);
+		if(my $type = $item->{type}) {
+			if($type eq 'erase') {
+				$rb->eraserect($item->{rect}, $item->{pen});
+			} else {
+				$log->errorf("Unknown writequeue type %s", $type);
+			}
+		} else {
+			$rb->text_at($item->{line}, $item->{col}, $item->{text}, $item->{pen});
+		}
 	}
 }
 
@@ -147,19 +155,289 @@ sub on_key {
 	if($ev->type eq 'key') {
 		if($ev->str eq 'Enter') {
 			$self->pty->write("\n");
+			$self->push_text("\n");
 		}
 	} elsif($ev->type eq 'text') {
 		$self->pty->write($ev->str);
+		$self->push_text($ev->str);
 	}
 	$self->redraw;
 }
 
+sub pen { $_[0]->{pen} //= $_[0]->get_style_pen->as_mutable }
+
+my %csi_map = (
+	m => sub {
+		my ($self, @param) = @_;
+		for(@param) {
+			if($_ == 0) {
+				$log->debug("SGR reset");
+				delete $self->{pen};
+			} elsif($_ == 1) {
+				$log->debug("SGR bold");
+				$self->pen->chattr(b => 1);
+			} elsif($_ == 2) {
+				$log->debug("SGR halfbright");
+			} elsif($_ == 4) {
+				$log->debug("SGR underscore");
+				$self->pen->chattr(u => 1);
+			} elsif($_ == 5) {
+				$log->debug("SGR blink");
+			} elsif($_ == 7) {
+				$log->debug("SGR reverse");
+				$self->pen->chattr(rv => 1);
+			} elsif($_ == 10) {
+				$log->debug("SGR primary font");
+			} elsif($_ == 11) {
+				$log->debug("SGR first alt");
+			} elsif($_ == 12) {
+				$log->debug("SGR second alt");
+			} elsif($_ == 21) {
+				$log->debug("SGR double-underline");
+			} elsif($_ == 22) {
+				$log->debug("SGR normal intensity");
+				$self->pen->chattr(b => 0);
+			} elsif($_ == 24) {
+				$log->debug("SGR disable underline");
+				$self->pen->chattr(u => 0);
+			} elsif($_ == 25) {
+				$log->debug("SGR disable blink");
+			} elsif($_ == 27) {
+				$log->debug("SGR disable RV");
+				$self->pen->chattr(rv => 0);
+			} elsif($_ >= 30 && $_ <= 37) {
+				my $fg = $_ - 30;
+				$log->debugf("SGR fg = %d", $fg);
+				$self->pen->chattr(fg => $fg);
+			} elsif($_ == 38) {
+				$log->debug("SGR underscore on, default fg");
+				$self->pen->chattr(fg => $self->get_style_pen->getattr('fg'));
+				$self->pen->chattr(u => 1);
+			} elsif($_ == 39) {
+				$log->debug("SGR underscore off, default fg");
+				$self->pen->chattr(fg => $self->get_style_pen->getattr('fg'));
+				$self->pen->chattr(u => 0);
+			} elsif($_ >= 40 && $_ <= 47) {
+				my $bg = $_ - 30;
+				$log->debugf("SGR bg = %d", $bg);
+				$self->pen->chattr(bg => $bg);
+			} elsif($_ == 49) {
+				$log->debug("SGR bg = default");
+				$self->pen->chattr(bg => $self->get_style_pen->getattr('bg'));
+			} else {
+				$log->warnf("SGR unknown parameter %s", $_);
+			}
+		}
+	},
+	H => sub {
+		my ($self, $line, $col) = @_;
+		$line //= 0;
+		$col //= 0;
+		$log->debug("SGR CUP %d, %d", $line, $col);
+		$self->{terminal_line} = $line - 1;
+		$self->{terminal_col} = $col - 1;
+	},
+	J => sub {
+		my ($self, $type) = @_;
+		$type //= 0;
+		$log->debug("SGR ED %d", $type);
+		@{$self->{writable}} = ();
+		$self->{terminal_line} = 0;
+		$self->{terminal_col} = 0;
+		$self->redraw;
+	},
+	K => sub {
+		my ($self, $type) = @_;
+		$type //= 0;
+		$log->debugf("SGR EL %d", $type);
+		push @{$self->{writable}}, {
+			type => 'erase',
+			pen  => $self->pen->as_immutable,
+			rect => Tickit::Rect->new(
+				top  => $self->terminal_line,
+				left => (
+					$type == 0
+					? $self->terminal_col
+					: 0
+				),
+				cols => (
+					$type == 1
+					? $self->terminal_col
+					: -1
+				),
+				lines => 1,
+			)
+		};
+		$self->redraw;
+	}
+);
+
+sub csi_map {
+	my ($self, $action) = @_;
+	return undef unless exists $csi_map{$action};
+	sub { $csi_map{$action}->($self, @_) }
+}
+
+use constant {
+	NORMAL => 0,
+	ESC => 1,
+	CSI => 2,
+};
+
 sub handle_terminal_output {
 	my ($self, $buf) = @_;
 	$log->debugf("We have %d bytes of exciting new PTY data to examine", length $$buf);
+	for($$buf) {
+		my $mode = NORMAL;
+		BREAKOUT:
+		while(1) {
+			# CAN/SUB abort escape sequence - i.e. switch to normal mode immediately
+			if(/\G\x18/gc) {
+				$log->debugf("CAN, bail out of escape mode (was %d)", $mode);
+				$mode = NORMAL;
+				redo BREAKOUT;
+			} elsif(/\G\x1A/gc) {
+				$log->debugf("SUB, bail out of escape mode (was %d)", $mode);
+				$mode = NORMAL;
+				redo BREAKOUT;
+			} elsif(/\G\x07/gc) {
+				$log->debug("BEEP");
+				redo BREAKOUT;
+			} elsif(/\G\x08/gc) {
+				$log->debug("Backspace");
+				redo BREAKOUT;
+			} elsif(/\G([\x0A\x0B\x0C])/gc) {
+				$log->debugf("Linefeed of some description (%s)", ord $1);
+				$self->push_text("\n");
+				redo BREAKOUT;
+			} elsif(/\G\x0D/gc) {
+				$log->debug("CR");
+				redo BREAKOUT;
+			} elsif(/\G\x0E/gc) {
+				$log->debug("Activate G1 character set");
+				redo BREAKOUT;
+			} elsif(/\G\x0F/gc) {
+				$log->debug("Activate G0 character set");
+				redo BREAKOUT;
+			} elsif(/\G\x7F/gc) {
+				$log->debug("DEL (ignored)");
+				redo BREAKOUT;
+			} elsif(/\G\x9B/gc) {
+				$log->debug("CSI");
+				$mode = CSI;
+				redo BREAKOUT;
+			}
+
+			if($mode == NORMAL) {
+				if(/\G([\x20-\x7F]+)/gc) {
+					$log->debugf("Text sequence: %s", $1);
+					$self->push_text($1);
+				} elsif(/\G\x1B/gc) {
+					$log->debugf("Escape sequence: %s", sprintf '%v02x', substr $_, pos, 8);
+					$mode = ESC;
+				} else {
+					$log->debugf("No characters of interest found, must be text: %s", substr $_, pos() // 0, -1);
+					last BREAKOUT
+				}
+			} elsif($mode == ESC) {
+				if(/\Gc/gc) {
+					$log->debug("ESC: Reset");
+					$mode = NORMAL;
+				} elsif(/\GD/gc) {
+					$log->debug("ESC: Linefeed");
+					$mode = NORMAL;
+				} elsif(/\GE/gc) {
+					$log->debug("ESC: Newline");
+					$mode = NORMAL;
+				} elsif(/\GH/gc) {
+					$log->debug("ESC: Set tab stop");
+					$mode = NORMAL;
+				} elsif(/\GM/gc) {
+					$log->debug("ESC: Reverse line feed");
+					$mode = NORMAL;
+				} elsif(/\GZ/gc) {
+					$log->debug("ESC: DEC ident");
+					$mode = NORMAL;
+				} elsif(/\G\[/gc) {
+					$log->debug("ESC: CSI");
+					$mode = CSI;
+				} elsif(/\G7/gc) {
+					$log->debug("ESC: DECSC");
+					$self->push_state;
+					$mode = NORMAL;
+				} elsif(/\G8/gc) {
+					$log->debug("ESC: DECSC");
+					$self->pop_state;
+					$mode = NORMAL;
+				} else {
+					$log->debugf("Some other ESC thing: %s", substr $_, pos() // 0, 1);
+					$mode = NORMAL;
+				}
+			} elsif($mode == CSI) {
+				if(/\G\??([\d;]*)(.)/gc) {
+					my ($action) = $2;
+					my @param = split /;/, $1;
+					$log->debugf("CSI: %s with %d parameters: %s", $action, 0 + @param, join ',', @param);
+					if(my $code = $self->csi_map($action)) {
+						$code->(@param);
+					} else {
+						$log->debugf("Unknown CSI action %s, had parameters: %s", $action, join ',', @param);
+					}
+					$mode = NORMAL;
+				} else {
+					$log->debugf("We are unknown CSI! %s (%s)", substr($_, pos()//0, 8), sprintf '%v02x', substr($_, pos()//0, 8));
+					$mode = NORMAL;
+				}
+			}
+		}
+	}
 	my $data = substr $$buf, 0, length($$buf), '';
-	push @{$self->{writable}}, split /\n/, $data;
+#	push @{$self->{writable}}, split /\n/, $data;
+	$self->redraw;
 	length $$buf;
+}
+
+sub push_state {
+	my ($self) = @_;
+	push @{$self->{dec_state}}, {
+		pen => $self->pen->as_mutable,
+		line => $self->terminal_line,
+		col => $self->terminal_col
+	};
+	$self
+}
+
+sub pop_state {
+	my ($self) = @_;
+	return $self unless my $state = pop @{$self->{dec_state}};
+	$self->{pen} = $state->{pen};
+	$self->{terminal_line} = $state->{line};
+	$self->{terminal_col} = $state->{col};
+	$self
+}
+
+sub terminal_line { $_[0]->{terminal_line} //= 0 }
+sub terminal_col { $_[0]->{terminal_col} //= 0 }
+
+sub push_text {
+	my ($self, $txt) = @_;
+	for($txt) {
+		if(/\G\n/gc) {
+			++$self->{terminal_line};
+			$self->{terminal_col} = 0;
+		} elsif(/\G([[:print:]]+)/gc) {
+			my $chunk = $1;
+			push @{$self->{writable}}, {
+				text => $chunk,
+				line => $self->terminal_line,
+				col  => $self->terminal_col,
+				pen  => $self->pen->as_immutable,
+			};
+			$self->{terminal_col} += textwidth $chunk;
+		} else {
+			$log->warnf("Unknown thing in text: %s", substr $_, pos()//0);
+		}
+	}
 }
 
 }
