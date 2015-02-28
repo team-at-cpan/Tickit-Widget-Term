96d0fa84982f2c7517bbedec20ded65ed672db22
Tom Molesworth <tom@perlsite.co.uk>
2015-02-28 17:28:09 +0000

    Frame and focus
    git-svn-id: http://entitymodel.com/svn/trunk/Tickit-Widget-Term@2951 c5bf8c3b-bf20-4ed3-b413-d5547a49f493

--- tickit-tester.pl
+++ tickit-tester.pl
@@ -501,13 +501,22 @@ use Tickit::Async;
 use IO::Async::Loop;
 use Log::Any qw($log);
 use Log::Any::Adapter qw(Stderr);
+use Tickit::Widget::Frame;
 
 my $loop = IO::Async::Loop->new;
 $loop->add(
 	my $tickit = Tickit::Async->new
 );
-$tickit->set_root_widget(
-	Tickit::Widget::Term->new(loop => $loop)
+my $frame = Tickit::Widget::Frame->new(
+	child => my $term = Tickit::Widget::Term->new(loop => $loop),
+	title => 'shell',
+	style => {
+		linetype => 'single'
+	},
 );
+$tickit->set_root_widget(
+	$frame
+);
+$term->take_focus;
 $tickit->run;
 
